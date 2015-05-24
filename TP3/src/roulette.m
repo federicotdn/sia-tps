33293d4be963a8b08b07a56683583c204d63ee2f
riveign <riveign@gmail.com>
2015-05-23 22:27:22 -0300

    tounament selection

--- TP3/src/roulette.m
+++ TP3/src/roulette.m
@@ -22,4 +22,11 @@ function ans = roulette_check(r, cumulative)
 	ans = i-1;
 endfunction
 
+function relative_fitness = relative_fitness(fitnesses)
+	relative_fitness = zeros(1,size(fitnesses)(2));
+	for i = 1:size(relative_fitness)(2)
+		relative_fitness(i) = fitnesses(i)/sum(fitnesses);
+	endfor
+endfunction
+
 roulette([4,3,6,8,2,4],4)
