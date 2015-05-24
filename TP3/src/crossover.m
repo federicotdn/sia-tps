80c96d0edbcc3e8fc418d0e565e2b5d7f2b775ef
riveign <riveign@gmail.com>
2015-05-24 01:10:18 -0300

    fix one point cross

--- TP3/src/crossover.m
+++ TP3/src/crossover.m
@@ -1,4 +1,5 @@
-function offsprings = one_point_cross(mother, father, k)
+function offsprings = one_point_cross(mother, father)
+	k = ceil(rand*length(mother));
 	son = [mother(1:(k-1)),father(k:end)];
 	doughter = [father(1:(k-1)),mother(k:end)];
 	offsprings = {son,doughter};
