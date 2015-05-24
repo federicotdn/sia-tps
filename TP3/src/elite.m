7d51f63c5fa98e661e6e79c2bbcf110d275d7dc1
Javier Fraire <javier.fraire@wolox.com.ar>
2015-05-24 01:19:50 -0300

    moved selection methods to selection.m

--- TP3/src/elite.m
+++
@@ -1,7 +0,0 @@
-function selected = elite(fitnesses, k)
-	selected = [];
-	sorted = sort(fitnesses);
-	for i =  (length(fitnesses)-k+1):length(fitnesses)
-		selected(end+1) = find(sorted(i) == fitnesses)(1);
-	endfor
-endfunction
