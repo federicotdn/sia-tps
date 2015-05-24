70f90636add514e2e0a478dcb84fb05ba4ba7a6b
riveign <riveign@gmail.com>
2015-05-24 03:32:18 -0300

    first replacement almost functional

--- TP3/src/replacement.m
+++ TP3/src/replacement.m
@@ -1,3 +1,21 @@
-function population = first_replacement(individuals, k)
+function population = first_replacement(genetic)
+	offsprings = {};
+	do
+		if genetic.select == 'elite'
+			selected = elite(genetic.individuals.fitnesses,2);
+		elseif genetic.select == 'roulette'
+			selected = roulette(genetic.individuals.fitnesses,2);
+		elseif genetic.select == 'tournament_deterministic'
+			selected = elite(genetic.individuals.fitnesses,2,genetic.tournament_contenders);
+		elseif genetic.select == 'mixed'
+			selected = elite(genetic.individuals.fitnesses,2, genetic.mixed_elite, genetic.mixec_reoulette);
+		endif
+		mother = genetic.individuals{selected(1)};
+		father = genetic.individuals{selected(2)};
+		new_offsprings = genetic.cross(mother,father);
+		for i = 1:length(new_offsprings)
+			offsprings{end+1} = genetic.mutation(new_offsprings{i});
+		endfor
+	while length(offsprings) < length(genetic.individuals)
 
 endfunction
