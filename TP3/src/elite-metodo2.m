f66b8211a9b2c824eaeb28fbb71094ca20385989
Federico T <ftedinur@itba.edu.ar>
2015-05-26 21:07:44 -0300

    elite+metodo2 ahora usa elite para reemplazo, ploteos

--- TP3/src/elite-metodo2.m
+++ TP3/src/elite-metodo2.m
@@ -15,7 +15,7 @@ k = 2 * 4; % must be even
 N = length(genetic.individuals.fitnesses);
 
 % Elite + Metodo de Reemplazo 2
-while generations < 1000
+while generations < 100000
 	selected = elite(genetic.individuals.fitnesses, k);
 
 	indices = (1: (k / 2)) * 2;
@@ -27,11 +27,13 @@ while generations < 1000
 		children = feval(genetic.cross_function, mother, father);
 
 		for i = 1:length(children)
-			new_weights{end + 1} = mutate_classic(children{i}, 0.4, 0.25);
+			%new_weights{end + 1} = mutate_non_uniform(children{i}, generations, N, 10000, 0.1, 0.25);
+			new_weights{end + 1} = mutate_classic(children{i}, 0.05, 0.25);
 		end
 	end
 
-	retained_old_weights = {genetic.individuals.weights{randperm(N, N - k)}};
+	%retained_old_weights = {genetic.individuals.weights{randperm(N, N - k)}}; % random
+	retained_old_weights = {genetic.individuals.weights{elite(genetic.individuals.fitnesses, N - k)}}; %reemplazo con elite
 	for i = 1:length(retained_old_weights)
 		new_weights{end + 1} = retained_old_weights{i};
 	end
@@ -39,8 +41,26 @@ while generations < 1000
 	genetic.individuals.weights = new_weights;
 	genetic = calculate_fitnesses(genetic);
 
+	generations++;
+
 	prom = sum(genetic.individuals.fitnesses) / length(genetic.individuals.fitnesses);
 	disp(prom)
 
-	generations++;
+	if mod(generations, 50) == 0
+		[m, index] = max(genetic.individuals.fitnesses);
+		all_best(end + 1) = m;
+
+		best = genetic.individuals.weights{index};
+
+		r = genetic.range;
+		best2 = individual_array_to_cell_array(best, genetic.arch);
+
+		results = feed_forward(best2, r);
+
+		subplot (2, 1, 1)
+		plot(r, results, r, genetic.expected_outputs);
+		subplot (2, 1, 2)
+		plot(1:(generations/50), all_best);
+		refresh();
+	end
 end
